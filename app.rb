ffd75b5fbfe92215461f97b2ad8bf4090592ec1e
AnnieGregg <annie.r.gregg@gmail.com>
2018-08-14 19:11:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do 
-    "My name is Mimi"
+    "My name is Annie"
   end
 
   get '/hometown' do 
-    "My hometown is Western Springs"
+    "My hometown is Orland Hills"
   end
 
   get '/favorite-song' do
-    "My favorite song is Oh, Jeez"
+    "My favorite song is Drive"
   end
 end
